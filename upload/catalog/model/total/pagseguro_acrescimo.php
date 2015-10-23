4a4997975aee2bc732a5ac98576d055543377cc6
Valdeir Santana <valdeir.naval@gmail.com>
2015-10-23 20:05:36 -0200

    Adiciona suporte a acrescimo <multa, taxa, etc)

---
+++ upload/catalog/model/total/pagseguro_acrescimo.php
@@ -0,0 +1,57 @@
+<?php
+class ModelTotalPagSeguroAcrescimo extends Model {
+    
+    public function getTotal(&$total_data, &$total, &$taxes) {
+        
+        if (!isset($this->session->data['payment_method']['code'])) {
+            return false;
+        }
+        
+        $this->language->load('payment/pagseguro');
+        
+        $acrescimo = 0;
+        
+        $this->config->set('pagseguro_acrescimo_boleto', 4);
+        
+        if ($this->session->data['payment_method']['code'] == 'pagseguro_boleto') {            
+            if (preg_match('#%#', $this->config->get('pagseguro_acrescimo_boleto'))) {
+                $acrescimo = preg_replace('/[^\d\.]/', '', $this->config->get('pagseguro_acrescimo_boleto'));
+                
+                $acrescimo = (($acrescimo / 100) * $this->cart->getSubTotal());
+            } else {
+                $acrescimo = $this->config->get('pagseguro_acrescimo_boleto');
+            }
+        }
+        
+        if ($this->session->data['payment_method']['code'] == 'pagseguro_credito') {
+            if (preg_match('#%#', $this->config->get('pagseguro_acrescimo_cartao'))) {
+                $acrescimo = preg_replace('/[^\d\.]/', '', $this->config->get('pagseguro_acrescimo_cartao'));
+                
+                $acrescimo = (($acrescimo / 100) * $this->cart->getSubTotal());
+            } else {
+                $acrescimo = $this->config->get('pagseguro_acrescimo_cartao');
+            }
+        }
+        
+        if ($this->session->data['payment_method']['code'] == 'pagseguro_debito') {
+            if (preg_match('#%#', $this->config->get('pagseguro_acrescimo_debito'))) {
+                $acrescimo = preg_replace('/[^\d\.]/', '', $this->config->get('pagseguro_acrescimo_debito'));
+                
+                $acrescimo = (($acrescimo / 100) * $this->cart->getSubTotal());
+            } else {
+                $acrescimo = $this->config->get('pagseguro_acrescimo_debito');
+            }
+        }
+        
+        if ($acrescimo > 0) {
+            $total += $acrescimo;
+            
+            $total_data[] = array(
+                'code'       => 'pagseguro_desconto',
+                'title'      => sprintf($this->language->get('text_acrescimo'), $this->currency->format($acrescimo)),
+                'value'      => +$acrescimo,
+                'sort_order' => ($this->config->get('total_sort_order') - 1)
+            );
+        }
+    }
+}
