f72c508d7f29f0044e0c773a98d10e4c74df6c1a
Valdeir Psr <valdeir.naval@gmail.com>
2017-07-11 04:27:31 -0300

    bugfix user_token

--- upload/admin/controller/extension/payment/pagseguro_cartao.php
+++ upload/admin/controller/extension/payment/pagseguro_cartao.php
@@ -2,6 +2,6 @@
 class ControllerExtensionPaymentPagseguroCartao extends Controller {
 	
 	public function index() {
-		$this->response->redirect($this->url->link('extension/payment/pagseguro', 'token=' . $this->session->data['user_token'], true));
+		$this->response->redirect($this->url->link('extension/payment/pagseguro', 'user_token=' . $this->session->data['user_token'], true));
 	}
 }
