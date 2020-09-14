f6251bd67677d493f8916fec19efd9d064a4fae9
Valdeir S <valdeir.naval@gmail.com>
2020-09-14 20:37:17 -0300

    feat: instala o VuePress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,76 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Documentação',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#73ba40' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'opencart-extension/PagSeguro-Checkout-Transparente',
+    repoLabel: 'Contribua!',
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'opencart-extension/PagSeguro-Checkout-Transparente',
+    // if your docs are not at the root of the repo:
+    docsDir: '.',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'feature/doc',
+
+    editLinks: true,
+    editLinkText: 'Ajude-nos a melhorar esta página!',
+
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Central de Atendimento',
+        link: 'https://valdeir.dev'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/opencart-extension/PagSeguro-Checkout-Transparente'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: []
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://oqueeucomprei.files.wordpress.com/2017/04/pagseguro-logo-2.png
+tagline: Documentaçao para a extensao desenvolvida para OpenCart
+actionText: Começar →
+actionLink: /guide/
+features:
+- title: Open-Source
+  details: Aproveite nosso projeto de código aberto para usar, modificar e adaptar à sua necessidade.
+- title: Simplicity First
+  details: Instalação e configuração simples
+- title: Extensible
+  details: Crie seus próprios layouts ou utilize nosso SDK para customizar regras de negócios
+footer: Made by  with ❤️
+---
