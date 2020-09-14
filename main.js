4db10d9ef736fcec8d8d233e245f3bf541e92c2a
Valdeir S <valdeir.naval@gmail.com>
2020-09-14 19:43:29 -0300

    refactor: add funçao para disparar evento DOMContentLoaded
    bugfix: corrige nome de variavel

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function loadWidgetHelpDesk() {
   const script = document.createElement('script')
-  script.dataset.JsdEmbedded = true
+  script.dataset.jsdEmbedded = true
   script.dataset.key = 'c965996b-d570-489a-87c7-855a21779a85'
   script.dataset.baseUrl = 'https://jsd-widget.atlassian.com'
   script.src = 'https://jsd-widget.atlassian.com/assets/embed.js'
+  script.onload = () => {
+    var event = document.createEvent("Event")
+    event.initEvent("DOMContentLoaded", true, true)
+    window.document.dispatchEvent(event)
+  }
   document.head.appendChild(script)
 }
 
@@ -12,15 +17,16 @@ function loadAd() {
     .then((response) => response.json())
     .then((response) => {
       document.querySelectorAll('div.tab-pane').forEach((item, key) => {
-        const keyArr = (key + 1) % arr.length
+        const keyArr = (key + 1) % response.length
+        console.log(item, response[keyArr])
         
         item.insertAdjacentHTML('beforeEnd', response[keyArr])
       })
     })
-    .catch(() => {})
+    .catch(console.warn)
 }
 
 (() => {
   loadWidgetHelpDesk()
   loadAd()
-})
+})()
