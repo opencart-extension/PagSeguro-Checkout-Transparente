ef42e8382f0c198d9ae41bdb622e2a1505954e65
Valdeir S <valdeir.naval@gmail.com>
2020-09-23 01:41:47 -0300

    fix: corrige envio da telemetria e newsletter

--- upload/admin/controller/extension/payment/pagseguro.php
+++ upload/admin/controller/extension/payment/pagseguro.php
@@ -589,13 +589,19 @@ class ControllerExtensionPaymentPagseguro extends Controller
                 'data' => $data
             ];
 
+            ob_start();
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_USERAGENT, 'PagSeguro Checkout Transparente for OpenCart');
             curl_setopt($curl, CURLOPT_POST, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json'
+            ]);
+            curl_exec($curl);
             curl_close($curl);
+            ob_end_clean();
         }
     }
 
@@ -624,14 +630,19 @@ class ControllerExtensionPaymentPagseguro extends Controller
             $url .= '/' . $fields['ref'];
         }
 
+        ob_start();
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_USERAGENT, 'PagSeguro Checkout Transparente for OpenCart');
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json'
+        ]);
         curl_exec($curl);
         curl_close($curl);
+        ob_end_clean();
     }
 
     /**
