ee34bb088162cd9f6a5fe9a969aa2224d17dd59d
Valdeir S <valdeir.naval@gmail.com>
2020-09-24 22:26:28 -0300

    chore: remove arquivos da versao 1.x

--- upload/admin/controller/extension/payment/pagseguro_debito.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ControllerExtensionPaymentPagseguroDebito extends Controller {
-	
-	public function index() {
-		$this->response->redirect($this->url->link('extension/payment/pagseguro', 'user_token=' . $this->session->data['user_token'], true));
-	}
-}
