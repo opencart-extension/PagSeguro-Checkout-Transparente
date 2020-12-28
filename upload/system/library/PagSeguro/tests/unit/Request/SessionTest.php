6981e4567da91ac0180a3643b70f2847a7ea54e6
Valdeir S <valdeir.naval@gmail.com>
2020-12-28 19:47:06 -0300

    refactor: adiciona exceçao caso haja falha ao gerar sessao

--- upload/system/library/PagSeguro/tests/unit/Request/SessionTest.php
+++ upload/system/library/PagSeguro/tests/unit/Request/SessionTest.php
@@ -2,12 +2,21 @@
 
 use PHPUnit\Framework\TestCase;
 use ValdeirPsr\PagSeguro\Domains\Environment;
+use ValdeirPsr\PagSeguro\Domains\Logger\Logger;
 use ValdeirPsr\PagSeguro\Request\Factory;
 use ValdeirPsr\PagSeguro\Request\Session;
 use ValdeirPsr\PagSeguro\Exception\Auth as AuthException;
+use ValdeirPsr\PagSeguro\Exception\PagSeguroRequest as PagSeguroRequestException;
 
 class SessionTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        Logger::getInstance([
+            'enabled' => false
+        ]);
+    }
+
     /**
      * @test
      */
@@ -45,7 +54,7 @@ class SessionTest extends TestCase
     public function whenEnteringInvalidCredentialsShouldReturnAnError()
     {
         $this->expectException(AuthException::class);
-        
+
         $env = Environment::sandbox('pagseguro@valdeir.dev', '');
 
         $stub = $this->getMockBuilder(Session::class)
@@ -56,6 +65,27 @@ class SessionTest extends TestCase
         $stub->expects($this->any())
             ->method('buildUrl')
             ->willReturn('https://f3528d51-6219-4b80-8bd3-3ab112b8094f.mock.pstmn.io/v2/sessions/invalid');
+
+        $stub->generate();
+    }
+
+    /**
+     * @test
+     */
+    public function whenEnteringServerIsDownShouldReturnAnError()
+    {
+        $this->expectException(PagSeguroRequestException::class);
+
+        $env = Environment::sandbox('pagseguro@valdeir.dev', '');
+
+        $stub = $this->getMockBuilder(Session::class)
+            ->setConstructorArgs([$env])
+            ->setMethods(['buildUrl'])
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('buildUrl')
+            ->willReturn('https://f3528d51-6219-4b80-8bd3-3ab112b8094f.mock.pstmn.io/v2/sessions/invalid-2');
 
         $stub->generate();
     }
